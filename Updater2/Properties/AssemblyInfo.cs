e766ea23e996e8577b5706a90c65bcc990c58c32
Travis Nickles <nickles.travis@gmail.com>
2017-11-02 07:02:57 -0500

    Updated copyright string

--- Updater2/Properties/AssemblyInfo.cs
+++ Updater2/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DS4Updater")]
-[assembly: AssemblyCopyright("Copyright © Jays2Kings 2014, 2015")]
+[assembly: AssemblyCopyright("Copyright © Jays2Kings 2014, 2015; Ryochan7 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
