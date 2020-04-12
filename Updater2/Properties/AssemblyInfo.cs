a8ce946df6d71119b9aa33e3c4d9999d06daf701
Travis Nickles <ryochan7@protonmail.com>
2020-04-12 11:35:05 -0500

    Version 1.4.2

--- Updater2/Properties/AssemblyInfo.cs
+++ Updater2/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DS4Updater")]
-[assembly: AssemblyCopyright("Copyright © Jays2Kings 2014, 2015; Ryochan7 2017, 2018, 2019")]
+[assembly: AssemblyCopyright("Copyright © Jays2Kings 2014, 2015; Ryochan7 2017, 2018, 2019, 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1")]
-[assembly: AssemblyFileVersion("1.4.1")]
+[assembly: AssemblyVersion("1.4.2")]
+[assembly: AssemblyFileVersion("1.4.2")]
